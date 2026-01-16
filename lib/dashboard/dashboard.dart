2ba80fd2d5df1b1e625317e302d2c1e862ee2a65
asyhnrz <nurazkiyahaisyah02@gmail.com>
2026-01-16 19:40:10 +0700

    card keuangan

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -28,10 +28,72 @@ class DashboardScreen extends StatelessWidget {
               'Ringkasan Keuangan Anda',
               style: TextStyle(color: Colors.grey),
             ),
+            SizedBox(height: 15),
+            
+            Row(
+              children: [
+                SummaryCard(
+                  title: 'Total Pemasukan',
+                  value: 'Rp -',
+                  color: Colors.green,
+                ),
+                SizedBox(width: 8),
+                SummaryCard(
+                  title: 'Total Pengeluaran',
+                  value: 'Rp -',
+                  color: Colors.red,
+                ),
+                SizedBox(width: 8),
+                SummaryCard(
+                  title: 'Total Saldo',
+                  value: 'Rp -',
+                  color: Colors.blue,
+                ),
+              ],
+            ),
             SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SummaryCard extends StatelessWidget {
+  final String title;
+  final String value;
+  final Color color;
+
+  const SummaryCard({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              'Transaksi Terbaru',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              title,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              value,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
